9b8c1f486f7b3607bb8d6994475429f370b2dc21
kirill1347678@outlook.com <kirill1347678@outlook.com>
2022-10-26 17:52:52 +0300

    1

---
+++ pz_10/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace pz_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "qwertyuiopasdfghjklzxcvbnmйцукенгшщзхъфывапролджэячсмитьбю ";
+
+            char[] chars = txt.ToCharArray();
+            string newtxt = string.Empty;
+            bool isFind = false;
+
+            while (true)
+            {
+                string str = Console.ReadLine();
+                str.ToLower();
+
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (str.Contains(chars[i]))
+                    {
+                        if (!isFind)
+                        {
+                            newtxt = str;
+
+                            if (newtxt != string.Empty)
+                            {
+                                isFind = true;
+                            }
+                        }
+
+                    }
+                }
+                if (str == string.Empty)
+                {
+                    Console.WriteLine(newtxt);
+                    break;
+                }
+            }
+        }
+    }
+}
