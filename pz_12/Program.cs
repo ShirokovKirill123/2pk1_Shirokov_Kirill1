868542038108e178cfce7cadb517fcc95b41a16e
kirill1347678@outlook.com <kirill1347678@outlook.com>
2022-11-15 19:49:38 +0300

    2

---
+++ pz_12/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace pz_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int summa;
+            int[] nums = { 123, 234, 456, 724, 879 };
+            //int[] arr2 = new int[3] { 1, 2, 3 };
+            //int[] arr3 = new int[3] { 2, 3, 4 };
+            //int[] arr4 = new int[3] { 4, 5, 6 };
+            //int[] arr5 = new int[3] { 7, 2, 4 };
+            //int[] arr6 = new int[3] { 8, 7, 9 };
+            //Console.WriteLine("Полученный массив:");
+            //for (int i = 0; i < nums.Length; i++)
+            //{               
+            //    Console.Write(nums[i] + "\t");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("Полученная суммма цифр массива:");
+            //GetSummOfArray(arr2, out summa);
+            //Console.Write(summa + "\t");
+            //GetSummOfArray(arr3, out summa);
+            //Console.Write(summa + "\t");
+            //GetSummOfArray(arr4, out summa);
+            //Console.Write(summa + "\t");
+            //GetSummOfArray(arr5, out summa);
+            //Console.Write(summa + "\t");
+            //GetSummOfArray(arr6, out summa);
+            //Console.Write(summa + "\t");
+
+
+            int[] res = GetSummOfArray(nums);
+            foreach (int i in res) Console.Write(i + " ");
+            Console.ReadLine();
+        }
+        static int[] GetSummOfArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int r1 = arr[i] / 100; //1
+                int r2 = (arr[i] - r1 * 100) / 10; //2
+                int r3 = (arr[i] - r1 * 100 - r2 * 10) / 1; //3
+                // temp += r / 
+
+                arr[i] = r1 + r2 + r3;
+            }
+            return arr;
+            //results = 0;
+            //foreach (int i in arr)
+            //{
+            //    results += i;
+            //}
+            //return results;
+        }
+    }
+}
---
+++ pz_13/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace pz_12
+{
+    internal class Program
+    {                   
+            static void RootsCount( out int A,  out int B,  out int C)
+            {
+                Console.WriteLine("Введите вещественное значение a:");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите вещественное значение b:");
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите вещественное значение c:");
+                double c = Convert.ToDouble(Console.ReadLine());
+                double D = (b * b) - 4 * a * c;
+                if (D > 0)
+                {
+                    A = 2;
+                }
+                else if (D == 0)
+                {
+                    A = 1;
+                }
+                else
+                {
+                    A = 0;                    
+                }
+                Console.WriteLine("Введите второе вещественное значение a:");                           
+                double a1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите второе вещественное значение b:");
+                double b1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите второе вещественное значение c:");
+                double c1 = Convert.ToDouble(Console.ReadLine());
+                double D1 = (b1 * b1) - 4 * a1 * c1;
+                if (D1 > 0)
+                {
+                    B = 2;
+                }
+                else if (D1 == 0)
+                {
+                    B = 1;
+                }
+                else
+                {
+                    B = 0;
+                }
+
+                Console.WriteLine("Введите третье вещественное значение a:");                
+                double a2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите третье вещественное значение b:");
+                double b2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите третье вещественное значение c:");
+                double c2 = Convert.ToDouble(Console.ReadLine());
+                double D2 = (b2 * b2) - 4 * a2 * c2;
+                if (D2 > 0)
+                {
+                    C = 2;
+                }
+                else if (D2 == 0)
+                {
+                    C = 1;
+                }
+                else
+                {
+                    C = 0;
+                }
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Квадратное уравнение имеет вид: ax^2 + bx + c = 0");
+                int A, B, C;
+                RootsCount(out A, out B, out C);
+                Console.WriteLine($"A имеет {A} корня(-ней),B имеет {B} корня(-ней),C имеет {C} корня(-ней)");
+            }        
+    }
+}
